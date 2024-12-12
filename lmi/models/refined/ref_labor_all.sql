c67e116581c456252e6c9ea57d079df7d3649572
GregoryTomy <95327355+GregoryTomy@users.noreply.github.com>
2024-12-12 20:46:25 +0000

    analysis eda

--- lmi/models/refined/ref_labor_all.sql
+++ lmi/models/refined/ref_labor_all.sql
@@ -24,22 +24,33 @@ with
         from stg_2022
     ),
 
-    labor_deduped as (select distinct * from labor_combined)
+    labor_deduped as (select distinct * from labor_combined),
+
+    labor_aggregated as (
+        select
+            year,
+            case
+                when month in (1, 2, 3)
+                then 1
+                when month in (4, 5, 6)
+                then 2
+                when month in (7, 8, 9)
+                then 3
+                when month in (10, 11, 12)
+                then 4
+            end as quarter,
+            sum(labor_force) as total_labor_force,
+            sum(labor_force_unemployed) as total_labor_force_unemployed
+        from labor_deduped
+        where area_name = 'Colorado' and period_type = 3 and is_adjusted = true
+        group by year, quarter
+
+    )
 
 select
-    year,
-    case
-        when month in (1, 2, 3)
-        then 1
-        when month in (4, 5, 6)
-        then 2
-        when month in (7, 8, 9)
-        then 3
-        when month in (10, 11, 12)
-        then 4
-    end as quarter,
-    labor_force,
-    labor_force_unemployed,
-    (labor_force_unemployed / labor_force) as unemployment_rate_calc,
-from labor_deduped
-where area_name = 'Colorado' and period_type = 3 and is_adjusted = true
+    *,
+    (
+        total_labor_force_unemployed::numeric / total_labor_force::numeric
+    ) as unemployment_rate_calc
+from labor_aggregated
+order by year, quarter
